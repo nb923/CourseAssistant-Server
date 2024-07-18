0e3256a4c43d28018aa5552166b99e45c06bd8f8
wavejpeg <jasantiagob9@gmail.com>
2024-07-18 15:39:37 -0400

    Store DF paths in env file

--- routing/main.py
+++ routing/main.py
@@ -1,10 +1,21 @@
 import pandas as pd
 from fastapi import FastAPI
+import os
+from dovenv import load_dotenv
+
+load_dotenv()
 
 print(pd.__version__)
 
-Data_Dictionary = pd.read_excel('/Users/josesantiago/Downloads/Data Values/DataDictionary.xlsx')
-print(Data_Dictionary.head())
+
+
+Data_Dictionary = pd.read_excel(os.getenv('DATA_DICT_PATH'))
+
+SASCourseNotes_SubjectNotes = pd.read_excel(os.getenv('COURSE_NOTES_PATH'))
+
+SASCourseCodes = pd.read_excel(os.getenv('COURSE_CODES_PATH'))
+
+SASCoursesCoreCodes_PreReq = pd.read_excel(os.getenv('COURSE_PREREQS_PATH'))
 
 app = FastAPI()
 
