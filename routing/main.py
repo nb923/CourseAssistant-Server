09aa634961031e2cedff6988016523134d692b61
wavejpeg <jasantiagob9@gmail.com>
2024-07-18 15:02:16 -0400

    Add python excel import test

--- routing/main.py
+++ routing/main.py
@@ -1,4 +1,10 @@
+import pandas as pd
 from fastapi import FastAPI
+
+print(pd.__version__)
+
+Data_Dictionary = pd.read_excel('/Users/josesantiago/Downloads/Data Values/DataDictionary.xlsx')
+print(Data_Dictionary.head())
 
 app = FastAPI()
 
